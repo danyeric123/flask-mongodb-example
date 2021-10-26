d39732c12c90cae826cf2c792f2240ddc62318f0
danyeric123 <dnagarpowers@gmail.com>
2021-10-26 14:49:01 -0400

    refactor to mongoengine

---
+++ models.py
@@ -0,0 +1,6 @@
+from mongoengine import Document, ListField, StringField, URLField
+from mongoengine.fields import BooleanField
+
+class Todo(Document):
+  content = StringField(required=True, max_length=70)
+  complete = BooleanField(default=False)
