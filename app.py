f288dc1feb633a7a61fbe59d3aa9cd45c0edad3f
danyeric123 <dnagarpowers@gmail.com>
2021-10-26 14:48:36 -0400

    refactor to mongoengine

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 from flask import Flask, render_template,request
 from flask.helpers import url_for
-from flask_pymongo import PyMongo
+from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
-import datetime
 from bson.objectid import ObjectId
-from bson import json_util
 import os
-import json
 
 from werkzeug.utils import redirect
+
+from models import Todo
 
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = DATABASE_URL
-mongo = PyMongo(app)
-
-todos = mongo.db.todos
+# app.config['MONGO_URI'] = DATABASE_URL
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'flaskmongo',
+    'host': DATABASE_URL
+}
+mongo = MongoEngine()
+mongo.init_app(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
   if request.method == 'POST':
     new_todo = request.form.get('new-todo')
-    todos.insert_one({'content' : new_todo, 'complete' : False})
-  all_todos = todos.find()
+    Todo(content=new_todo).save()
+  all_todos = Todo.objects()
   return render_template('index.html', todos=all_todos)
 
-@app.route('/delete/<oid>')
-def delete_todo(oid):
-  todos.delete_one({'_id': ObjectId(oid)})
+@app.route('/delete/<id>')
+def delete_todo(id):
+  Todo.objects(id=id).delete()
   return redirect(url_for('index'))
 
 if __name__ == '__main__':
