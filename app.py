d0ed4e03cc2b6525a6fb0d33f694480857c2daa8
danyeric123 <dnagarpowers@gmail.com>
2022-03-07 12:43:26 -0500

    format by black

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask.helpers import url_for
 from flask_mongoengine import MongoEngine
 from flask_cors import CORS
@@ -12,47 +12,48 @@ from models import Todo
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 app = Flask(__name__)
 CORS(app)
 
 # app.config['MONGO_URI'] = DATABASE_URL
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'flaskmongo',
-    'host': DATABASE_URL
-}
+app.config["MONGODB_SETTINGS"] = {"db": "flaskmongo", "host": DATABASE_URL}
 mongo = MongoEngine()
 mongo.init_app(app)
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-  if request.method == 'POST':
-    new_todo = request.form.get('new-todo')
-    Todo(content=new_todo).save()
-  all_todos = Todo.objects()
-  return all_todos.to_json()
+    if request.method == "POST":
+        new_todo = request.form.get("new-todo")
+        Todo(content=new_todo).save()
+    all_todos = Todo.objects()
+    return all_todos.to_json()
 
-@app.route('/delete/<id>')
+
+@app.route("/delete/<id>")
 def delete_todo(id):
-  Todo.objects(id=id).delete()
-  return redirect(url_for('index'))
+    Todo.objects(id=id).delete()
+    return redirect(url_for("index"))
 
-@app.route('/todos/<id>')
+
+@app.route("/todos/<id>")
 def show_todo(id):
-  todo = Todo.objects.get(id=id)
-  return render_template('show.html',todo=todo)
+    todo = Todo.objects.get(id=id)
+    return render_template("show.html", todo=todo)
 
-@app.route('/todos/<id>/edit',methods=['POST','GET'])
+
+@app.route("/todos/<id>/edit", methods=["POST", "GET"])
 def update(id):
-  if request.method == 'POST':
-    completed = True if request.form.get("complete") else False
-    Todo.objects(id=id).update(
-      content=request.form.get("content"),
-      complete=completed
-      )
-    return redirect(url_for('index'))
-  todo = Todo.objects.get(id=id)
-  return render_template('update.html',todo=todo) 
+    if request.method == "POST":
+        completed = True if request.form.get("complete") else False
+        Todo.objects(id=id).update(
+            content=request.form.get("content"), complete=completed
+        )
+        return redirect(url_for("index"))
+    todo = Todo.objects.get(id=id)
+    return render_template("update.html", todo=todo)
 
-if __name__ == '__main__':
-  app.run()
+
+if __name__ == "__main__":
+    app.run()
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from mongoengine import Document, ListField, StringField, URLField
 from mongoengine.fields import BooleanField
 
+
 class Todo(Document):
-  content = StringField(required=True, max_length=70)
-  complete = BooleanField(default=False)
+    content = StringField(required=True, max_length=70)
+    complete = BooleanField(default=False)
